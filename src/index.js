ea4a2d2d0552bd83b10a1051cce0f0c0bb93b3cc
vladislavgribkov <vladislavgrbkovq@yandex.by>
2020-09-23 09:10:50 +0300

    feat: complited task towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let outArraya = [];
+    if(matrix === undefined){
+        return [];
+      }
+    matrix.forEach((element, i)=> {
+      if(i%2 === 0){
+        outArraya.push(...element.sort((a,b)=>a-b));
+      }else{
+        outArraya.push(...element.sort((a,b)=>b-a));
+      }
+    });
+    return outArraya;
 }
